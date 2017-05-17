d86f8be8fb88bf57bd2d3e7610d2f379c6688817
Christophe Opoix <christophe.opoix@gmail.com>
2017-05-17 10:33:23 +0200

    using communicate to read from stdin

--- easyca/utils/shell.py
+++ easyca/utils/shell.py
@@ -5,11 +5,15 @@ import subprocess
 def call(cmd, write=None):
     """ Call command and returns stdout as an utf-8 string. """
 
+    print(cmd)
+
     popen = subprocess.Popen(cmd,
                              shell=True,
                              stdout=subprocess.PIPE,
                              stdin=subprocess.PIPE)
-    result = popen.stdout.read().decode("utf-8")
-    if write != None:
-        popen.stdin.write(write.encode('utf-8'))
+    if write is not None:
+        write_bstring = write.encode('utf-8')
+    else:
+        write_bstring = None
+    result = popen.communicate(write_bstring)[0].decode('utf-8')
     return result
